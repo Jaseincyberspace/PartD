882040b58a1e420ac1d90d7ddc823f383e45deb8
Jaseincyberspace <jase.norton@gmail.com>
2020-01-07 21:18:43 +1300

    Line 4 added - Best day of week

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		System.out.println("Hi, my name is Jason Norton");
 		System.out.println("My favourite colour is Green");
 		System.out.println("My favourite mode of transportation is motorbike");
+		System.out.println("The best day of the week is... which ever day I get off work that week");
 
 	}
 
