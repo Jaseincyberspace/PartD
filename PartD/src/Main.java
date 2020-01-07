a4986f9dc6e04ea4fada0ee8d4681d5eec836450
Jaseincyberspace <jase.norton@gmail.com>
2020-01-07 21:17:06 +1300

    Line 3 added - Transportation

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Jason Norton");
 		System.out.println("My favourite colour is Green");
+		System.out.println("My favourite mode of transportation is motorbike");
 
 	}
 
